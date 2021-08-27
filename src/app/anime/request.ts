d93ca66e599d92f22874896eeba67aa703f1ddf0
Javier Beltrán <javiierb6@gmail.com>
2021-08-26 20:29:52 -0600

    avance

--- src/app/anime/request.ts
+++ src/app/anime/request.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -12,9 +12,15 @@ export async function getData<T>(
   path: string,
   params?: HttpParams
 ): Promise<send<T>> {
+  localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE0MCIsIm5iZiI6MTYzMDAwMTIyOSwiZXhwIjoxNjMyNTkzMjI5LCJpYXQiOjE2MzAwMDEyMjl9.2IlU82X8kgoBulXXOKszDJYirRQgxzlCoo5yxNbAWRY')
+  const headers = new HttpHeaders()
+  .set('Authorization', `Bearer ${localStorage.getItem('token')}`)
+  .set('Content-Type', 'application/json')
+  .set('Accept', 'application/json');
   const data = await http
     .get<any>(`https://api.aniapi.com/v1/${path}`, {
       params: params,
+      headers: headers
     })
     .pipe(
       map((data) => ({
